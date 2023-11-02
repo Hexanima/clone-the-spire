a22864afefac18665e25c521d3eb2c053bed5b22
Hexanima <nicolasfilippelli22@gmail.com>
2023-11-02 00:43:51 -0300

    Archivos iniciales

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Unit player = new Unit();
+Unit enemy = new Unit();
+
+Console.WriteLine($"Enemy Health: {enemy.Health}\nEnemy Shield: {enemy.Shield}\n");
+
+Console.WriteLine($"Player attacks enemy for {player.Strength + 5} damage");
+player.Attack(enemy, 5);
+Console.WriteLine($"Enemy Health: {enemy.Health}\nEnemy Shield: {enemy.Shield}\n");
+
+player.Strength += 2;
+Console.WriteLine("Player gains 2 strength\n");
+
+Console.WriteLine($"Player attacks enemy for {player.Strength + 5} damage");
+player.Attack(enemy, 5);
+Console.WriteLine($"Enemy Health: {enemy.Health}\nEnemy Shield: {enemy.Shield}\n");
+
+Console.WriteLine($"Player attacks enemy for {player.Strength + 5} damage");
+player.Attack(enemy, 5);
+Console.WriteLine($"Enemy Health: {enemy.Health}\nEnemy Shield: {enemy.Shield}\n");
+
+class Unit
+{
+    public Unit()
+    {
+        Health = 100;
+        Shield = 7;
+        Strength = 0;
+        Dexterity = 0;
+    }
+    public int Health { get; set; }
+    public int Shield { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public virtual void Attack(Unit unit, int damage)
+    {
+        int remainingDamage = damage + Strength - unit.Shield;
+        unit.Shield -= damage + Strength;
+        if (unit.Shield < 0)
+        {
+            unit.Shield = 0;
+        }
+        if (remainingDamage > 0)
+        {
+            unit.Health -= remainingDamage;
+        }
+    }
+
+    public virtual void Defend(int defense)
+    {
+        Shield += defense + Dexterity;
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Slay the Spire Clone
+
+Este es un proyecto en el cual intento copiar las mecanicas y jugabilidad del videojuego "Slay the Spire" en una aplicacion de consola.
+
+## Proposito
+
+El proposito de este proyecto es aprender los conceptos de Programacion Orientada a Objetos (POO), manejo de mecanicas y sistema de turnos, y mejorar la resolucion de problemas.
+
+## ¿Como lo ejecuto?
+
+### Visual Studio Community
+
+Si tienes Visual Studio Community, puedes ingresar directamente este proyecto en una carpeta, ya que cuenta con los archivos de proyecto.
+
+### Visual Studio Code
+
+Si tienes Visual Studio Code, necesitaras instalar la extension oficial [C# Dev Kit](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit), la cual provee varias herramientas para poder crear y gestionar proyectos .NET.
+
+Una vez instalada la extension, ingresa a la carpeta del proyecto, abre la consola y ejecuta el comando `dotnet run`, el cual compilará y ejecutará la aplicación en la misma consola. 
